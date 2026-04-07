070823547a25c36f1aa88a4a72d4d82d2e2f83cf
Joudacasa <joudacasagames@gmail.com>
2026-04-07 13:10:38 -0300

    Delete assets directory

--- assets/src/helper.jsx
+++
@@ -1,8 +0,0 @@
-export const Helpers = {
-    getTime() {
-        return new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    },
-    copyToClipboard(text) {
-        navigator.clipboard.writeText(text);
-    }
-};
